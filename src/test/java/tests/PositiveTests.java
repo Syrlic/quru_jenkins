8315a01fdea7c2a8eefb96895fc1df425abb6b64
Syrlic <oklonina@gmail.com>
2021-09-28 09:57:32 +0400

    tests for jenkins

---
+++ src/test/java/tests/PositiveTests.java
@@ -0,0 +1,21 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PositiveTests {
+
+    @Test
+    void test01(){
+        assertTrue(true);
+    }
+    @Test
+    void test02(){
+        assertTrue(true);
+    }
+    @Test
+    void test03(){
+        assertTrue(true);
+    }
+}
