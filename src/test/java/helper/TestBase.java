dcdfb79d22fc609ef95682aa64954b58caa33937
Syrlic <oklonina@gmail.com>
2021-10-27 10:31:16 +0400

    added config file

--- src/test/java/helper/TestBase.java
+++ src/test/java/helper/TestBase.java
@@ -1,6 +1,8 @@
 package helper;
 
 import com.codeborne.selenide.Configuration;
+import org.aeonbits.owner.Config;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -8,6 +10,8 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.net.MalformedURLException;
 
 public class TestBase {
+
+public static CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
 
     @BeforeAll
     public static void setup() throws MalformedURLException {
@@ -19,15 +23,17 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+        String formatURL = String.format("https://$s:$s@selenoid.autotests.cloud/wd/hub/",
+                config.login(), config.password());
+        Configuration.remote = formatURL;
 
     }
 
-    @AfterEach
-    public void tearDown() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
-    }
+//    @AfterEach
+//    public void tearDown() {
+//        Attach.screenshotAs("Last screenshot");
+//        Attach.pageSource();
+//        Attach.browserConsoleLogs();
+//        Attach.addVideo();
+//    }
 }
